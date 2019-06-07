e71a0561045fbbcf597318780df8854b195ba084
LeonardoCorreiaGon <leonardocorreiagon@gmail.com>
2019-06-06 22:33:51 -0300

    update api service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,35 +1,55 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
-// import { Observable, throwError } from 'rxjs';
-// import { retry, catchError } from 'rxjs/operators';
-// -------------------------------------------------------------------------- //
-// import { Clients } from '../services/clients';
+import { Company } from './company';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
-  providedIn: 'root'
+     providedIn: 'root'
 })
 
 export class ApiService {
 
-  apiURL = "http://gpa.herokuapp.com/api";
-  res = {};
+     private api = "http://127.0.0.1:8000/";
 
-  constructor(private http: HttpClient) { }
+     constructor(private http: HttpClient) { }
 
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
-  } 
+     httpOptions = {
+          headers: new HttpHeaders({
+               'Content-Type': 'application/json'
+          })
+     }
 
-  getData(extURL){
-    this.http.get(this.apiURL+extURL).subscribe(data => {
-      this.res = data;
-      console.log(data);
-    });
-  }
+     // getCompany (): Observable<Company[]> {
+     //      return this.http.get(this.api + 'company?limit=10', {}, {})
+     //           .then(r => {
+     //                console.log(r)
+     //           })
+     // }
+
+     // postCompany (c) {
+     //      return this.http.post<Company>(this.api + 'company/register/', c, {})
+     //           .subscribe(r => {
+     //                console.log(r)
+     //           })
+     // } 
+
+     // deleteCompany (i) {
+     //      return this.http.delete(this.api + 'company/' + i)
+     //           .subscribe(r => {
+     //                console.log(r)
+     //           })
+     // }
+
+     // postAppointement (a) {
+     //      return this.http.post(this.api + 'company/register/appointement', a, {})
+     //           .subscribe(r => {
+     //                console.log(r)
+     //           })
+     // }
+
+
+
 
 
 
--- src/app/services/clients.ts
+++
@@ -1,7 +0,0 @@
-export class Clients {
-    id: string;
-    first_name: string;
-    last_name: string;
-    email: string;
-    phone: number;
-}
